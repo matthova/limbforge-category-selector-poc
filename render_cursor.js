ed4061487824ed516ba593b9795ef342dcf56a3f
Kaitlyn Hova <kaitlynhova@gmail.com>
2016-10-28 15:11:16 -0700

    updates

--- render_cursor.js
+++ render_cursor.js
@@ -2,17 +2,36 @@
 // replace px numbers with %% of total height
 
 function render_cursor(x, y) {
+  console.log(y);
   var level = y;
   if (y < 40) {
-    level = 20;
+    level = 0;
+    $("#info").text("0");
+    console.log("Level = " + level + " y = " + y);
   } else if (y < 80) {
-    level = 80;
-  } else if (y < 120) {
-    level = 120;
+    level = 60;
+    $("#info").text("Shoulder Disarticulation");
+    console.log("Level = " + level + " y = " + y);
+  } else if ((y > 80) && (y < 140)) {
+    level = 100;
+    $("#info").text("50% Trans Humeral");
+    console.log("Level = " + level + " y = " + y);
+  } else if ((y > 140) &&(y < 190)) {
+    level = 150;-
+    $("#info").text("Elbow Disarticulation");
+    console.log("Level = " + level + " y = " + y);
+  } else if ((y > 190) && (y < 210)) {
+    level = 200;
+    $("#info").text("Trans Metacarpal");
+    console.log("Level = " + level + " y = " + y);
+  } else if ((y > 210) && (y < 235)) {
+    level = 220;
+    $("#info").text("Wrist area");
+  } else if ((y > 235) && (y < 240)) {
+    level = 230;
+    $("#info").text("Hand area");
   }
-  if (y > 270){
-    y = 270
-  }
+
   $("#top").css("clip", "rect(0px, 350px, " + level + "px, 0px)");
   $("#bar").css("top", y + "px");
 }
